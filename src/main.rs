20315c29575843e96174040d92c2e751f98893f5
SPFab <42518661+SPFabGerman@users.noreply.github.com>
2023-07-29 22:23:23 +0200

    Package file is now parsed with Tera

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::ffi::OsStr;
-use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::process::{Command, Stdio};
+use std::process::{Command, ExitCode, Stdio};
+use tera::{Context, Tera};
 
 fn run_cmd<T: AsRef<OsStr>>(cmd: &[T]) -> Result<(), Box<dyn Error>> {
     if cmd.is_empty() {
-	return Ok(())
+        return Ok(());
     }
-    
+
     // let cmd_ret = Command::new(&cmd[0]).args(&cmd[1..]).status()?;
     let cmd_ret = Command::new("echo").arg(">").args(cmd).status()?; // For debugging purposes
     if !cmd_ret.success() {
@@ -20,7 +21,7 @@ fn run_cmd<T: AsRef<OsStr>>(cmd: &[T]) -> Result<(), Box<dyn Error>> {
 
 fn run_cmd_with_list<T: AsRef<OsStr>>(cmd: &[T], list: &[T]) -> Result<(), Box<dyn Error>> {
     if cmd.is_empty() || list.is_empty() {
-	return Ok(())
+        return Ok(());
     }
 
     // let cmd_ret = Command::new(&cmd[0]).args(&cmd[1..]).args(list).status()?;
@@ -33,7 +34,7 @@ fn run_cmd_with_list<T: AsRef<OsStr>>(cmd: &[T], list: &[T]) -> Result<(), Box<d
 
 fn get_packages_from_command<T: AsRef<OsStr>>(cmd: &[T]) -> Result<Vec<String>, Box<dyn Error>> {
     if cmd.is_empty() {
-	return Err(Box::from("No command was specified!"))
+        return Err(Box::from("No command was specified!"));
     }
 
     let mut cmd_proc = Command::new(&cmd[0])
@@ -54,10 +55,75 @@ fn get_packages_from_command<T: AsRef<OsStr>>(cmd: &[T]) -> Result<Vec<String>, 
     Ok(package_list)
 }
 
-fn get_packages_from_config(path: &Path) -> io::Result<Vec<String>> {
-    let file = File::open(path)?;
-    let mut package_list: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
+fn get_group_packages(args: &HashMap<std::string::String, tera::Value>) -> Result<tera::Value, tera::Error> {
+    let groupname = match args.get("name") {
+        Some(val) => val.clone(),
+        None => return Err("No group was specified".into()),
+    };
+    let groupname = match groupname {
+        tera::Value::String(s) => s,
+        _ => return Err("Groupname is no string!".into()),
+    };
+    let mut packages = match get_packages_from_command(&["pacman", "-Sqg", &groupname[..]]) {
+        Ok(p) => p,
+        Err(_) => return Err("Packages in group could not be found.".into()),
+    };
+
+    if let Some(v) = args.get("except") {
+        let to_unlist = match v.clone() {
+            tera::Value::String(s) => vec![s],
+            tera::Value::Array(arr) => {
+                let mut a = Vec::new();
+                for v in arr {
+                    match v {
+                        tera::Value::String(s) => a.push(s),
+                        _ => return Err("Array does contain non-String elements!".into()),
+                    }
+                }
+                a
+            }
+            _ => return Err("except-Keyword can only contain Strings or Array of Strings.".into()),
+        };
+        packages = compare_lists_only_in_first(&packages, &to_unlist);
+    }
+
+    Ok(packages.join("\n").into())
+}
+
+fn error_pretty_print(err: &dyn Error, skip_first: bool) -> String {
+    let mut skip_first = skip_first;
+    let mut s = Vec::new();
+    let mut err: Option<&dyn Error> = Some(err);
+    while let Some(e) = err {
+        if !skip_first {
+            s.push(e.to_string());
+        }
+        skip_first = false;
+        err = e.source();
+    }
+
+    // If the Error string contains newlines, assume we have a multiline error and display it on its own lines.
+    if s.concat().contains('\n') {
+        format!("\n{}", s.join("\n"))
+    } else {
+        s.join(": ")
+    }
+}
+
+fn get_packages_from_config(path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
+    // Initialize Tera and load config file from path
+    let mut tera = Tera::default();
+    tera.add_template_file(path, Some("config_file"))?;
+
+    // Setup functions and variables
+    let context = Context::new();
+    tera.register_function("group", Box::new(get_group_packages));
+
+    // Read config file and map to array
+    let render = tera.render("config_file", &context)?;
+    let mut package_list: Vec<String> = render.lines().map(String::from).collect();
     package_list.sort_unstable();
+
     Ok(package_list)
 }
 
@@ -204,13 +270,29 @@ impl SystemConfigSyncronizer for ThreeStateSyncronizer {
     }
 
     fn report_up_diff(&self, up_diff: &Self::UpDiff) {
-        println!("{} {}", self.to_install_report_msg, up_diff.to_install.join(", "));
-        println!("{} {}", self.to_mark_explicit_report_msg, up_diff.to_mark_explicit.join(", "));
+        if !up_diff.to_install.is_empty() {
+            println!("{} {}", self.to_install_report_msg, up_diff.to_install.join(", "));
+        }
+        if !up_diff.to_mark_explicit.is_empty() {
+            println!(
+                "{} {}",
+                self.to_mark_explicit_report_msg,
+                up_diff.to_mark_explicit.join(", ")
+            );
+        }
     }
 
     fn report_down_diff(&self, down_diff: &Self::DownDiff) {
-        println!("{} {}", self.to_remove_report_msg, down_diff.to_remove.join(", "));
-        println!("{} {}", self.to_mark_dependency_report_msg, down_diff.to_mark_dependency.join(", "));
+        if !down_diff.to_remove.is_empty() {
+            println!("{} {}", self.to_remove_report_msg, down_diff.to_remove.join(", "));
+        }
+        if !down_diff.to_mark_dependency.is_empty() {
+            println!(
+                "{} {}",
+                self.to_mark_dependency_report_msg,
+                down_diff.to_mark_dependency.join(", ")
+            );
+        }
     }
 
     fn sync_up(&self, up_diff: Self::UpDiff) -> Result<(), Box<dyn Error>> {
@@ -230,19 +312,24 @@ impl SystemConfigSyncronizer for ThreeStateSyncronizer {
     }
 
     fn post_sync(&self) -> Result<(), Box<dyn Error>> {
-	let orphans = get_packages_from_command(&self.get_orphans_cmd)?;
+        let orphans = get_packages_from_command(&self.get_orphans_cmd)?;
         run_cmd_with_list(&self.remove_cmd, &orphans)
     }
 }
 
-fn main() -> Result<(), String> {
+fn main() -> ExitCode {
     let config_packages = match get_packages_from_config(Path::new("current_packages")) {
-        Err(e) => return Err(format!("Could not open config file: {}", e)),
+        Err(e) => {
+            eprintln!("Could not parse config file: {}", error_pretty_print(e.as_ref(), true));
+            return ExitCode::FAILURE;
+        }
         Ok(packages) => packages,
     };
     let pacman_config = new_pacman();
     if let Err(e) = pacman_config.sync_up_down(&config_packages) {
-        return Err(format!("Error syncronizing: {}", e));
+        eprintln!("Error syncronizing: {}", error_pretty_print(e.as_ref(), false));
+        return ExitCode::FAILURE;
     }
-    Ok(())
+
+    ExitCode::SUCCESS
 }
